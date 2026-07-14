a865ab5fc8f53d84742dd1dda327d229d067270b
Skyl3lazer <derekjjjj@gmail.com>
2026-07-14 16:03:57 -0400

    Fixed clarities and scars merging

--- Source/Hediff_PsycheClarity.cs
+++ Source/Hediff_PsycheClarity.cs
@@ -4,15 +4,13 @@ namespace Psyche
 {
     public class Hediff_PsycheClarity : HediffWithComps
     {
-        private static int nextUiKey;
-
         private int uiKey;
 
         public override string LabelBase => PsycheUtility.BandedLabel(this);
 
         public override string LabelInBrackets => string.Empty;
 
-        public override int UIGroupKey => uiKey != 0 ? uiKey : (uiKey = ++nextUiKey);
+        public override int UIGroupKey => uiKey != 0 ? uiKey : (uiKey = PsycheUtility.NextUiGroupKey());
 
         public override bool TryMergeWith(Hediff other) => false;
     }
